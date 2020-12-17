e1aec3d6c4791cc659ad12ef8f6b9a61d2e82a5e
royhadad98 <roy@roundtrip.co.il>
2020-12-17 09:47:50 +0200

    build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,7 +1,7 @@
 declare type GenericObject<T> = {
     [key: string]: T;
 };
-declare type Unpacked<T> = T extends (infer U)[] ? U : T extends (...args: any[]) => infer U ? U : T extends Promise<infer U> ? U : T;
+declare type Unpacked<T> = T extends Promise<infer U> ? U : T;
 declare type ResultsDictionary<T> = {
     [P in keyof T]: Unpacked<T[P]>;
 };
