e4c551d3d229f1ee4ddbe600f0cfb93bb77ab076
royhadad98 <roy@roundtrip.co.il>
2020-12-17 09:46:54 +0200

    simplified the type Unpacked

--- src/types.ts
+++ src/types.ts
@@ -5,13 +5,9 @@ type GenericObject<T> = {
 // Unpacks the type
 // official typescript docs:
 // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#type-inference-in-conditional-types
-type Unpacked<T> = T extends (infer U)[]
+type Unpacked<T> = T extends Promise<infer U>
     ? U
-    : T extends (...args: any[]) => infer U
-        ? U
-        : T extends Promise<infer U>
-            ? U
-            : T;
+    : T;
 
 type ResultsDictionary<T> = {
     [P in keyof T]: Unpacked<T[P]>
